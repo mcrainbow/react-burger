60b6748c99a6431104bca1d07e0951ac0b189041
mcrainbow <stefro97@yandex.ru>
2025-10-22 15:47:48 +0300

    delete dublicated react-burger folder

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchIngredients()).unwrap();
+    dispatch(fetchIngredients());
   }, [dispatch]);
 
   return (
